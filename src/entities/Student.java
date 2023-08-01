ff44669a60a408851931d02b950214cc43db8983
Fernando Felício <fernandofelicio.dev@gmail.com>
2023-08-01 13:11:43 -0300

    Criando metodo totalAverage

--- src/entities/Student.java
+++ src/entities/Student.java
@@ -1,5 +1,16 @@
 package entities;
 
 public class Student {
+	
+	public String nameStudent;
+	public double firstSemesterGrade;
+	public double secondSemesterGrade;
+	public double thirdSemesterGrade;
+	
+	//Criando metodo totalAverage
+	public double totalAverage() {
+		double totalAverage = 100 - (firstSemesterGrade + secondSemesterGrade + thirdSemesterGrade);
+		return totalAverage;
+	}
 
 }
